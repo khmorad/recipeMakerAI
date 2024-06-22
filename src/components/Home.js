ba61b7fd3b35e85bd10aedee811f955450c94578
khmorad <khakho.morad@gmail.com>
2024-06-22 12:46:45 -0700

    improved intro + added picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import Navbar from './Navbar'
-import '../stylings/Home.css'
-import Intro from './Intro'
+import React from 'react';
+import Navbar from './Navbar';
+import '../stylings/Home.css';
+import Intro from './Intro';
+
 export default function Home() {
   return (
     <>
-    <Navbar/>
-    <Intro/>
-    <div>Home</div>
+      <Navbar />
+      <Intro />
+      <div style={{ marginTop: '20px', textAlign: 'center' }}>Home</div>
     </>
-  )
+  );
 }
