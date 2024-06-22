34f9a75937060d846cdb1aa97f408b13d7f9189f
khmorad <khakho.morad@gmail.com>
2024-06-22 10:45:01 -0700

    navbar created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import Navbar from './Navbar'
+import '../stylings/Home.css'
 export default function Home() {
   return (
+    <>
+    <Navbar/>
     <div>Home</div>
+    </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
-import React from 'react'
-
-export default function Navbar() {
+import React from 'react';
+import '../stylings/Navbar.css'
+const Navbar = () => {
+  const logoStyle = {
+    height: '40px',
+    width: 'auto',
+    borderRadius: '50%',
+  }
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="logo">
+        <a href="#"><img src="https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo.png" alt="Logo" style={logoStyle} /></a>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
 }
+
+export default Navbar;
