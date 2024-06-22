48c57d1a9e2162b12ba8b01144421d34141522c6
khmorad <khakho.morad@gmail.com>
2024-06-22 13:58:13 -0700

    intro fix

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import Navbar from './Navbar'
+import '../stylings/Home.css'
+import Intro from './Intro'
+import IngredientInputPage from "./IngredientInputPage";
 export default function ImageUpload() {
   return (
-    <div>ImageUpload</div>
+    <>
+    <Navbar/>
+    <Intro/>
+    <IngredientInputPage />
+
+    </>
   )
 }
